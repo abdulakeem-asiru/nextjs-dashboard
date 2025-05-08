b67a67c4f452c588c361c2cca19a2de6c2da3764
abdulakeem-asiru <asiruabdulakeem@gmail.com>
2025-05-08 12:55:51 +0100

    dashboard  update

--- app/dashboard/invoices/create/page.tsx
+++ app/dashboard/invoices/create/page.tsx
@@ -1,7 +1,6 @@
 import Form from '@/app/ui/invoices/create-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchCustomers } from '@/app/lib/data';
-export async function createInvoice(formData: FormData) {}
  
 export default async function Page() {
   const customers = await fetchCustomers();
